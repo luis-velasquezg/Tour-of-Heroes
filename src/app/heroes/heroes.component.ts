eb2b9c8ea841a65790e8f4fbaa2105a08a5d7bcb
luis.velasquezg <luis.velasquezg@udea.edu.co>
2018-08-12 17:27:53 -0500

    heroService - Commit 2 (Message Component & Message Service)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,15 +12,21 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
+  heroes: Hero[];
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
