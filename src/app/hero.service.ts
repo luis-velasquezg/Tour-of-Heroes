a55639712d39a3c69adbde3a256aa3fdf83e3c4e
luis.velasquezg <luis.velasquezg@udea.edu.co>
2018-08-12 22:29:29 -0500

    routingModule - Commit 3

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,12 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
