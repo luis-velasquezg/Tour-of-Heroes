14742d67dde0b49111d9048ee86458dca8ba6853
luis.velasquezg <luis.velasquezg@udea.edu.co>
2018-08-13 15:06:53 -0500

    httpService - Commit 2 (Update Hero)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,14 @@ import { InMemoryDataService }  from './in-memory-data.service';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
